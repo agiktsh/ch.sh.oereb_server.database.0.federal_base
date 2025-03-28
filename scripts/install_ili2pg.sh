36ff8c958a79a5e5a5b0886596d0a60b1e75ab56
vvmruder <vvmruder@users.noreply.github.com>
2025-03-28 11:03:10 +0100

    Update install_ili2pg.sh

--- scripts/install_ili2pg.sh
+++ scripts/install_ili2pg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-curl -o /tmp/ili2pg-$ILI2PG_VERSION.zip https://83.166.138.19/ili2pg/ili2pg-${ILI2PG_VERSION}.zip
+curl -o /tmp/ili2pg-$ILI2PG_VERSION.zip -g -6 https://downloads.interlis.ch/ili2pg/ili2pg-${ILI2PG_VERSION}.zip
 rm -rf /tmp/ili2pg
 mkdir /tmp/ili2pg
 unzip -d /tmp/ili2pg /tmp/ili2pg-${ILI2PG_VERSION}.zip
